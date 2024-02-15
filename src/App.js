9707d6b96603614886926c10e67eaa46ad7ab0f8
Pedro Botega <botegapedro@gmail.com>
2024-02-15 17:07:05 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import { Fab } from '@mui/material';
 import './App.css';
+import styled from '@emotion/styled';
+
+const OptionsContainer = styled.div`
+  height: 200px;
+  overflow: auto;
+  width: min-content;
+  ::-webkit-scrollbar {
+    width: 4px;
+    border-radius: 8px;
+  }
+  ::-webkit-scrollbar-thumb {
+      background-color: #3498db;
+  }
+`
+
+const FabStyled = styled(Fab)`
+  margin-top: 16px;
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Fab color="primary" aria-label="add">
+          +
+        </Fab>
+        <OptionsContainer>
+          <FabStyled>
+            +
+          </FabStyled>
+          <FabStyled>
+            +
+          </FabStyled>
+          <FabStyled>
+            +
+          </FabStyled>
+          <FabStyled>
+            +
+          </FabStyled>
+          <FabStyled>
+            +
+          </FabStyled>
+        </OptionsContainer>
       </header>
     </div>
+
   );
 }
 
